6ba7046830e51ed05b6f7d1558b6303eaa64ef37
jchow97 <jeffreychow97@gmail.com>
2022-12-03 21:19:28 -0800

    Initial commit. Establish project structure.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# BasketballPredictor
+# Basketball Predictor
+
+This program aims to scrape historical data in order to identify factors relevant to game outcomes 
+and accurately predict the outcome of future games.
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    # name='sample',
+    # version='0.1.0',
+    # description='Sample package for Python-Guide.org',
+    # long_description=README.md,
+    # author='Jeffrey Chow',
+    # author_email='jeffreychow97@gmail.com',
+    # url='',
+    # license='LICENSE',
+    # packages=
+)
