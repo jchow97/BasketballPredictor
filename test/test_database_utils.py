6c2e080ed1dcef28700a5fbf2f9fd3339a5a6c56
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-06-07 18:40:25 -0700

    Resolve other weak warnings (2).

--- test/test_database_utils.py
+++ test/test_database_utils.py
@@ -5,6 +5,7 @@ class TestDatabaseHelpers(unittest.TestCase):
     def test_method(self):
         pass
 
+
 if __name__ == "__main__":
     unittest.main()
     print("Everything passed")
--- test/test_scraper.py
+++ test/test_scraper.py
@@ -9,12 +9,13 @@ class TestScraper(unittest.TestCase):
         self.test_scraper = Scraper()
         self.arbitrary_season_year = '2000'
 
-
     def test_scrape_nba_match(self):
-        self.test_scraper.scrape_nba_match('202204120BRK', 'https://www.basketball-reference.com/boxscores/202204120BRK.html')
+        self.test_scraper.scrape_nba_match('202204120BRK',
+                                           'https://www.basketball-reference.com/boxscores/202204120BRK.html')
 
     def test_scrape_nba_player(self):
-        self.test_scraper.scrape_nba_player('bambamo01', 'https://www.basketball-reference.com/players/b/bambamo01.html')
+        self.test_scraper.scrape_nba_player('bambamo01',
+                                            'https://www.basketball-reference.com/players/b/bambamo01.html')
 
 
 if __name__ == "__main__":
