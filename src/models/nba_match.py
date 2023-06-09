75dc5d9604fc816adbeef8db8c5d5307de9d4604
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-06-08 17:44:07 -0700

    Updated NBA match constructor.

--- src/models/nba_match.py
+++ src/models/nba_match.py
@@ -4,8 +4,11 @@ class NbaMatch:
     NBA Season Object.
     """
 
-    def __init__(self):
+    def __init__(self, game_code):
         """
-        Constructor
+        Constructor for match object used for model training and prediction.
+        :param game_code: Unique game code.
         """
-        pass
+        self.game_code = game_code
+        self.away_team = None
+        self.home_team = None
