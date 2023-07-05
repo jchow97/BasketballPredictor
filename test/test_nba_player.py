dbc421db71faba6b28ed5eededf3cd6168b7f87e
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-07-04 19:54:11 -0700

    Implemented test_update_bpm

--- test/test_nba_player.py
+++ test/test_nba_player.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from models.nba_player import NbaPlayer
 
@@ -8,5 +9,18 @@ class TestNbaPlayer(TestCase):
     def setUp(self):
         self.test_player = NbaPlayer("Test Player 1", "testplayer01")
 
-    def test_update_bpm(self):
-        self.fail()
+    @patch('models.nba_player.GamePlayerLog')
+    def test_update_bpm(self, mock_player_log):
+        type(mock_player_log).box_plus_minus = 10
+
+        self.test_player.update_bpm(mock_player_log)
+
+        self.assertEqual(1, self.test_player.games_played)
+        self.assertEqual(10, self.test_player.bpm_total)
+        self.assertEqual(10, self.test_player.bpm)
+
+        self.test_player.update_bpm(mock_player_log)
+
+        self.assertEqual(2, self.test_player.games_played)
+        self.assertEqual(20, self.test_player.bpm_total)
+        self.assertEqual(10, self.test_player.bpm)
